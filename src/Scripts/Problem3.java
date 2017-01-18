3385c01c943feba7fdaeb128c1b725e78c944e73
Samuel Beaubien <Samuel.Beaubien@Gmail.com>
2017-01-18 18:04:21 -0500

    implement findPrimeFactor method

--- src/Scripts/Problem3.java
+++ src/Scripts/Problem3.java
@@ -7,11 +7,51 @@ public class Problem3 {
 	public static void main(String[] args)
 	{
 	
-		ArrayList<Integer> listPrimes = generatePrimes(100);
-		
-		
+		ArrayList<Integer> listPrimes = findPrimeFactors(13195, 1);
 		
 	}
+	
+	
+	private static ArrayList<Integer> findPrimeFactors(int unFactoredNumber, int factorArray)
+	{
+		//Initialize list possible primes and list prime factor
+		ArrayList<Integer> arrayPrimes = generatePrimes((int)Math.sqrt(unFactoredNumber) * factorArray );
+		ArrayList<Integer> arrayPrimeFactors = new ArrayList<Integer>();
+		
+		int currentNumber = unFactoredNumber; 
+		
+		// currentNumber is divisible by a prime, currentNumber becomes result of division and 
+		// 
+		
+		for (int indexArrayPrimes = 0; indexArrayPrimes < arrayPrimes.size(); indexArrayPrimes++)
+		{
+			if (currentNumber % arrayPrimes.get(indexArrayPrimes) == 0)
+			{
+				arrayPrimeFactors.add(arrayPrimes.get(indexArrayPrimes));
+				currentNumber = currentNumber / arrayPrimes.get(indexArrayPrimes);
+			}
+		}
+		
+		
+		// Make sure the biggest have been found
+		if (currentNumber > 1)
+		{
+			arrayPrimeFactors = findPrimeFactors(unFactoredNumber, (factorArray + 1));
+		}
+		
+		
+		return arrayPrimeFactors;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	// Implementation of the sieve of Eratosthenes
