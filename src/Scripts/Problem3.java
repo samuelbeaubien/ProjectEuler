06b5d571787d7f5f06c40d9f2abff4e7b412ea9a
Samuel Beaubien <Samuel.Beaubien@Gmail.com>
2017-01-24 16:12:24 -0500

    Write removeMultiple and generatePrimes with BigIntegers, Add method sqrt(BigInteger)

--- src/Scripts/Problem3.java
+++ src/Scripts/Problem3.java
@@ -7,21 +7,50 @@ public class Problem3 {
 
 	public static void main(String[] args)
 	{
-	
-		long x = 13195;
-	
-		ArrayList<Integer> listPrimes = findPrimeFactors(13195, 1);
 		
-		System.out.println(listPrimes);
+		ArrayList<BigInteger> list = new ArrayList<BigInteger>();
+	
+		BigInteger x1 = new BigInteger("1");
+		list.add(x1);
+		BigInteger x2 = new BigInteger("2");
+		list.add(x2);
+		BigInteger x3 = new BigInteger("3");
+		list.add(x3);
+		BigInteger x4 = new BigInteger("4");
+		list.add(x4);
+		BigInteger x5 = new BigInteger("5");
+		list.add(x5);
+		BigInteger x6 = new BigInteger("6");
+		list.add(x6);
+		BigInteger x7 = new BigInteger("7");
+		list.add(x7);
+		BigInteger x8 = new BigInteger("8");
+		list.add(x8);
+		BigInteger x9 = new BigInteger("9");
+		list.add(x9);
+		BigInteger x10 = new BigInteger("100");
+		list.add(x10);
+
+		removeMultiple(list, x2);
+		
+		
+		ArrayList<BigInteger> list2 = generatePrimes(x10);
+		
+		
+		System.out.println(list2);
+		
+		//ArrayList<BigInteger> listPrimes = findPrimeFactors(x, 1);
+		
+		//System.out.println(listPrimes);
 		
 	}
+	/*
 	
-	
-	private static ArrayList<Integer> findPrimeFactors(long unFactoredNumber, int factorArray)
+	private static ArrayList<BigInteger> findPrimeFactors(BigInteger unFactoredNumber, int factorArray)
 	{
-		//Initialize list possible primes and list prime factor
+		//Initialize list of possible primes and list known factor
 		ArrayList<Integer> arrayPrimes = generatePrimes((int)Math.sqrt(unFactoredNumber) * factorArray );
-		ArrayList<Integer> arrayPrimeFactors = new ArrayList<Integer>();
+		ArrayList<Integer> arrayFactors = new ArrayList<Integer>();
 		
 		long currentNumber = unFactoredNumber; 
 		
@@ -32,7 +61,7 @@ public class Problem3 {
 		{
 			if (currentNumber % arrayPrimes.get(indexArrayPrimes) == 0)
 			{
-				arrayPrimeFactors.add(arrayPrimes.get(indexArrayPrimes));
+				arrayFactors.add(arrayPrimes.get(indexArrayPrimes));
 				currentNumber = currentNumber / arrayPrimes.get(indexArrayPrimes);
 			}
 		}
@@ -41,66 +70,97 @@ public class Problem3 {
 		// Make sure the biggest have been found
 		if (currentNumber > 1)
 		{
-			arrayPrimeFactors = findPrimeFactors(unFactoredNumber, (factorArray + 1));
+			arrayFactors = findPrimeFactors(unFactoredNumber, (factorArray + 1));
 		}
 		
 		
-		return arrayPrimeFactors;
+		return arrayFactors;
 	}
 	
 	
 	
+	*/
+	
+	
+	
+
 	
 	
 	
 	
-	
-	
-	
-	
-	
-	// Implementation of the sieve of Eratosthenes
-	
-	private static ArrayList<Integer> generatePrimes(int maxPrime)
+	/* Implementation of the sieve of Eratosthenes
+	* 1. Creates arrayNumbers with number from 2 to maxPrime
+	* 2. Start with first number in arrayNumbers (necessarily prime), put it in arrayPrimes, 
+	* remove all multiple of it in arrayNumber with removeMultiple
+	*/
+	private static ArrayList<BigInteger> generatePrimes(BigInteger maxPrime)
 	{
-		
-		// Create an array of number with the max number being the maxPrime
-		
-		ArrayList<Integer> arrayNumbers = new ArrayList<Integer>();
-		
-		int upperbound = (int) Math.sqrt(maxPrime);
-		
-		for (int counterNumber = 2; counterNumber <= maxPrime; counterNumber++)
+		// Create an array of BigInteger with the max number being the the square root maxPrime
+		ArrayList<BigInteger> arrayNumbers = new ArrayList<BigInteger>();
+		BigInteger upperbound = sqrt(maxPrime);
+		BigInteger BigOne = new BigInteger("1");
+		for (BigInteger counterNumber = new BigInteger("2"); counterNumber.compareTo(maxPrime) < 1; counterNumber = counterNumber.add(BigOne))
 		{
 			arrayNumbers.add(counterNumber);
 		}
 		
-		ArrayList<Integer> arrayPrimes = new ArrayList<Integer>();
-		
+		// Remove multiple until upperbound then just put remaining numbers in arrayPrimes
+		ArrayList<BigInteger> arrayPrimes = new ArrayList<BigInteger>();
 		while (arrayNumbers.size() > 0)
 		{
-			int currentNumber = arrayNumbers.get(0);
+			BigInteger currentNumber = arrayNumbers.get(0);
+			if (currentNumber.compareTo(upperbound) >= 0)
+			{
+				break;
+			}
 			removeMultiple(arrayNumbers, currentNumber);
 			arrayPrimes.add(currentNumber);
+		}
+	
+		// Add remaining number from arrayNumbers to arrayPrimes
+		if (arrayNumbers.size() > 0)
+		{
+			arrayPrimes.addAll(arrayNumbers);
 		}
 		
 		return arrayPrimes;
 		
 	}
 	
+
 	
-	private static ArrayList<Integer> removeMultiple(ArrayList<Integer> currentArray, int multiple)
+	// Removes all multiple of the argument 'multiple' from array currentArray
+	
+	private static void removeMultiple(ArrayList<BigInteger> currentArray, BigInteger multiple)
 	{
-		
+		BigInteger BigZero = new BigInteger("0");
 		for (int index = 0; index < currentArray.size(); index++)
 		{
-			if (currentArray.get(index) % multiple == 0)
+			if (currentArray.get(index).mod(multiple).equals(BigZero))
 			{
 				currentArray.remove(index);
+				index--;
 			}
 		}
-		
-		return currentArray;
+	}
+	
+	
+	
+	// Code from stackOverFlow user EdwardFalk (May 29 2013)
+	// Implements square root operation for BigIntger
+	
+	public static BigInteger sqrt(BigInteger x) {
+	    BigInteger div = BigInteger.ZERO.setBit(x.bitLength()/2);
+	    BigInteger div2 = div;
+	    // Loop until we hit the same value twice in a row, or wind
+	    // up alternating.
+	    for(;;) {
+	        BigInteger y = div.add(x.divide(div)).shiftRight(1);
+	        if (y.equals(div) || y.equals(div2))
+	            return y;
+	        div2 = div;
+	        div = y;
+	    }
 	}
 	
 	
